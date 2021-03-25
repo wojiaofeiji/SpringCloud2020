9e26e0d405848195a0ac0e7e630f2391f6a1f743
Fred <zpf8836@163.com>
2021-03-25 08:14:46 +0800

    add 83cloud-stream module

---
+++ cloud-83cloud-stream/cloud-83eureka-server7001/src/main/java/com/fredg/springcloud/EurekaMain7001.java
@@ -0,0 +1,17 @@
+package com.fredg.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @auther FredG.
+ * @date on 2021/3/24
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class);
+    }
+}
---
+++ cloud-83cloud-stream/cloud-83stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/service/impl/MessageProviderImpl.java
@@ -0,0 +1,35 @@
+package com.atguigu.springcloud.service.impl;
+
+import com.atguigu.springcloud.service.IMessageProvider;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.MessageChannel;
+
+import javax.annotation.Resource;
+import java.util.UUID;
+
+/**
+ * EnableBinding 定义消息的推送管道
+ *
+ * @author zzyy
+ * @version 1.0
+ * @create 2020/03/06
+ */
+@EnableBinding(Source.class)
+public class MessageProviderImpl implements IMessageProvider {
+
+    /**
+     * 消息发送管道
+     */
+    @Resource
+    private MessageChannel output;
+
+    @Override
+    public String send() {
+        String serial = UUID.randomUUID().toString();
+        output.send(MessageBuilder.withPayload(serial).build());
+        System.out.println("serial = " + serial);
+        return serial;
+    }
+}
