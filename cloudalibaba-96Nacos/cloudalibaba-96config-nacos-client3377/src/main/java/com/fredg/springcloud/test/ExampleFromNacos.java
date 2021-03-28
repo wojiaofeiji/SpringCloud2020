de2a1f21942f0d3e2b5d3ec88838b21f436e236b
Fred <zpf8836@163.com>
2021-03-28 18:17:43 +0800

    update 111sentinel module

---
+++ cloudalibaba-96Nacos/cloudalibaba-96config-nacos-client3377/src/main/java/com/fredg/springcloud/test/ExampleFromNacos.java
@@ -0,0 +1,57 @@
+package com.fredg.springcloud.test;
+
+import com.alibaba.nacos.api.NacosFactory;
+import com.alibaba.nacos.api.PropertyKeyConst;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.config.listener.Listener;
+import com.alibaba.nacos.api.exception.NacosException;
+
+import java.util.Properties;
+import java.util.concurrent.Executor;
+
+/**
+ * @auther FredG.
+ * @date on 2021/3/28
+ */
+public class ExampleFromNacos {
+
+
+    public static void main(String[] args) throws NacosException, InterruptedException {
+        String serverAddr = "192.168.1.111:8848";
+        String dataId = "nacos-config-client-dev.yml";
+        String group = "DEFAULT_GROUP";
+        Properties properties = new Properties();
+        properties.put(PropertyKeyConst.SERVER_ADDR, serverAddr);
+        ConfigService configService = NacosFactory.createConfigService(properties);
+        String content = configService.getConfig(dataId, group, 5000);
+        System.out.println(content);
+        configService.addListener(dataId, group, new Listener() {
+            @Override
+            public void receiveConfigInfo(String configInfo) {
+                System.out.println("recieve:" + configInfo);
+            }
+
+            @Override
+            public Executor getExecutor() {
+                return null;
+            }
+        });
+
+        boolean isPublishOk = configService.publishConfig(dataId, group, "content");
+        System.out.println(isPublishOk);
+
+        Thread.sleep(3000);
+        content = configService.getConfig(dataId, group, 5000);
+        System.out.println(content);
+
+        boolean isRemoveOk = configService.removeConfig(dataId, group);
+        System.out.println(isRemoveOk);
+        Thread.sleep(3000);
+
+        content = configService.getConfig(dataId, group, 5000);
+        System.out.println(content);
+        Thread.sleep(300000);
+
+    }
+
+}
